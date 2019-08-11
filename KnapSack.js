f15ae4c6c92ec81016666fd2df86feb422f3bdf3
Harris Law <lawharris92@gmail.com>
2019-08-11 12:22:36 +1000

    v1

---
+++ KnapSack.js
@@ -0,0 +1,83 @@
+// n = n items
+// val[] = value of items
+// W = capacity of the sack
+// wt[] = weight of items
+
+//var values = [130, 60, 120];
+//var weights = [10, 20, 30];
+
+var values = [60, 100,120];
+var weights = [10, 20, 30];
+var W = 50;
+
+
+function KnapSackGreedy(W, values, weights, n){
+	// Stop when there's no more space or items
+	if(W == -1 || n == -1){
+		return 0;
+	}
+	
+	console.log(W);
+	console.log(n);
+	console.log(weights[n]);
+	console.log("======================");
+	// If this one doesn't feed, jump to the next one
+	if(weights[n] >  W){
+		return KnapSackGreedy(W, values, weights, n-1);
+	} else{
+		return Math.max(values[n] + KnapSackGreedy(W-weights[n], values, weights, n-1), KnapSackGreedy(W, values, weights, n-1));
+	}
+	
+}
+
+//console.log(KnapSackGreedy(W,values,weights,values.length-1));
+
+var result = [];
+
+//Preoccupy with the first base value
+result[0] = {value:values[values.length-1], weight:weights[values.length-1]};
+function KnapSackDynamic(W, values, weights, main_header, comparison_header, result_length){	
+	if(main_header == -1 || comparison_header == -1){
+		return 0;
+	}
+	
+	//If the current new weight + the existing weight is bigger than threshold, then you choose the max by comparing the result from
+	//testing add the next comparing value, or change the next base value
+	if(parseInt(result[result_length].weight) + parseInt(weights[comparison_header]) > parseInt(W) && parseInt(comparison_header) >= 0){
+		//TODO: if 
+		if(main_header > 0 && comparison_header > 0){
+			result.push();
+			var length1 = result_length+1;
+			result[length1] = {value:values[main_header], weight:weights[main_header]};
+			result.push();
+			var length2 = length1+1;
+			result[length2] = {value:values[main_header-1], weight:weights[main_header-1]};
+			//We can skip some comparison here
+			return Math.max(KnapSackDynamic(W, values, weights, main_header, comparison_header-1, length1),KnapSackDynamic(W, values, weights, main_header-1, comparison_header, length2));
+		} else if (comparison_header > 0){
+			result.push();
+			var length = result_length+1;
+			result[length] = {value:values[main_header], weight:weights[main_header]};
+			return KnapSackDynamic(W, values, weights, main_header, comparison_header-1, length);
+		} else if (main_header > 0){
+			result.push();
+			var length = result_length+1;
+			result[length] = {value:values[main_header-1], weight:weights[main_header-1]};
+			return KnapSackDynamic(W, values, weights, main_header-1, comparison_header, length);
+		} else{
+			return 0;
+		}
+	} else{
+		//Keep adding weight until the existing bag exceeds the threshold
+		result[result_length] = {value: result[result_length].value+values[comparison_header], weight:result[result_length].weight+weights[comparison_header]};
+		return KnapSackDynamic(W, values, weights, main_header, comparison_header, result_length);
+	}
+}
+
+KnapSackDynamic(W,values,weights,values.length-1, values.length-1, 0);
+
+//Find the max after you have listed all combinations
+result.sort((a, b) => (a.value > b.value) ? 1 : -1)
+console.log(result);
+console.log("+++++++++++++++++++");
+console.log(result[result.length-1]);
